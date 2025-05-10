2fce53cdb11b211fc9925c2b862acdb05ffeb751
programacionparaaprender <yancel209@gmail.com>
2025-05-10 16:46:48 -0500

    agregando cambios

--- src/main/java/com/example/ejemplothymeleaf/model/Persona.java
+++ src/main/java/com/example/ejemplothymeleaf/model/Persona.java
@@ -1,14 +1,13 @@
 package com.example.ejemplothymeleaf.model;
 
-
-
-import javax.persistence.Column;
-
-import javax.persistence.Entity;
-import javax.persistence.GenerationType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Data
